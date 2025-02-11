abfaabb1f9f12110029383e86a7c4e7202a8d349
cN3rd <subs.in.tokyo@gmail.com>
2025-02-11 18:54:51 +0200

    Refactor arrows to use object pooling

--- Assets/Scripts/MainGame/Hazards/ArrowHazard.cs
+++ Assets/Scripts/MainGame/Hazards/ArrowHazard.cs
@@ -1,28 +1,76 @@
-using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Pool;
 
 public class ArrowHazard : MonoBehaviour
 {
-    public GameObject arrowPrefab;
+    static readonly Quaternion ArrowRotation = Quaternion.Euler(0, 180, 0);
+    [SerializeField] GameObject arrowPrefab;
     [SerializeField] float shootInterval;
-    private float shootIntervalLeft;
+    [SerializeField] float maxDistance = 50f;
+    readonly HashSet<(GameObject arrow, Vector3 startPos)> _activeArrows = new();
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    ObjectPool<GameObject> _arrowPool;
+
     void Start()
     {
-        shootIntervalLeft = shootInterval;
+        InitializeArrowPool();
+
+        StartCoroutine(ShootCoroutine());
+        StartCoroutine(TrackArrowsCorotuine());
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator TrackArrowsCorotuine()
     {
-        shootIntervalLeft -= Time.deltaTime;
-        if (shootIntervalLeft <= 0)
+        var estimatedCapacity = Mathf.CeilToInt(maxDistance / 5f); // a good estimate for now...
+        var arrowsToRelease = new List<(GameObject arrow, Vector3 startPos)>(estimatedCapacity);
+
+        while (true)
         {
-            ArrowObject arrow = Instantiate(arrowPrefab,transform.position,Quaternion.identity).GetComponent<ArrowObject>();
-            arrow.transform.Rotate(0,90,0);
-            arrow.transform.Rotate(0,90,0);
-            shootIntervalLeft = shootInterval;
+            arrowsToRelease.Clear();
+            foreach (var (arrow, startPos) in _activeArrows)
+                if ((startPos - arrow.transform.position).sqrMagnitude >= maxDistance * maxDistance)
+                    arrowsToRelease.Add((arrow, startPos));
+
+            foreach (var tuple in arrowsToRelease) _arrowPool.Release(tuple.arrow);
+
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
+
+    void InitializeArrowPool()
+    {
+        GameObject CreateArrowFunc()
+        {
+            return Instantiate(arrowPrefab, transform.position, ArrowRotation);
+        }
+
+        void OnGetArrow(GameObject arrow)
+        {
+            arrow.SetActive(true);
+            arrow.transform.position = transform.position;
+            _activeArrows.Add((arrow, transform.position));
+        }
+
+        void OnReleaseArrow(GameObject arrow)
+        {
+            arrow.SetActive(false);
+            arrow.transform.position = transform.position;
+            _activeArrows.RemoveWhere(tuple => tuple.arrow == arrow);
+        }
+
+        _arrowPool = new ObjectPool<GameObject>(CreateArrowFunc, OnGetArrow, OnReleaseArrow, Destroy);
+    }
+
+    IEnumerator ShootCoroutine()
+    {
+        var waitForInterval = new WaitForSeconds(shootInterval);
+        while (true)
+        {
+            yield return waitForInterval;
+
+            _arrowPool.Get();
         }
     }
 }
